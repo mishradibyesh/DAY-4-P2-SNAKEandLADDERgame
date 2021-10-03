0a624d3651660d4229356f477eb126d4b64a8eb5
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-03 13:55:18 +0530

    generating number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,9 @@
+import java.lang.*;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 	int Position = 0;		
+	double Throw=(Math.floor(Math.random() *10 ) %6)+1 ;
+	System.out.println("outcome is"+Throw);
 	}
 }
-
-
